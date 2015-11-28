478f512dbf402a1ea273d13bdf3b80f30ab27975
Ritika Ahuja <ritika7ahuja@gmail.com>
2015-11-28 17:07:57 -0500

    Front End Code Modified

---
+++ js/tabs.js
@@ -0,0 +1,17 @@
+﻿// Project Codename: Papertree
+// Codding By Anjali Verma
+// Version 1.0
+// (In Development Phase)
+
+
+
+$(function(){
+    $('.tab-section').hide();
+    $('#tabs a').bind('click', function(e){
+        $('#tabs a.current').removeClass('current');
+        $('.tab-section:visible').hide();
+        $(this.hash).show();
+        $(this).addClass('current');
+        e.preventDefault();
+    }).filter(':first').click();
+});
